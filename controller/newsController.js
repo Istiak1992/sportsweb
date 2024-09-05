617a998166fdbde237e2c06f951f9c5778b7a72c
istiak1992 <istiak.uiu.bd@gmail.com>
2024-09-05 23:29:10 +0600

    first commit

---
+++ controller/newsController.js
@@ -0,0 +1,32 @@
+const { scrapeNewsBlogs, scrapeBlogDetails } = require("../service/newsBlogsService");
+
+
+const getNewsBlogs = async (req, res) => {
+    const { clicks } = req.query;
+    try {
+        const clickCount = clicks ? parseInt(clicks) : 0;
+        const data = await scrapeNewsBlogs(clickCount);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching news blogs:', error);
+        res.status(500).json({ error: 'Failed to fetch news blogs' });
+    }
+};
+const getBlogDetails = async (req, res) => {
+    const { slug, cat, id } = req.params;
+
+
+    try {
+        const url = `https://cricket.one/${cat}/${slug}/${id}`;
+
+        const blogDetails = await scrapeBlogDetails(url);
+        res.json(blogDetails);
+    } catch (error) {
+        console.error('Error fetching blog details:', error);
+        res.status(500).json({ error: 'Failed to fetch blog details' });
+    }
+};
+module.exports = {
+    getNewsBlogs,
+    getBlogDetails
+ };
